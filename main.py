fcabf5a041b490edf489c1334241cd6a9496cb75
Benjamin Li <76795078+benjason921@users.noreply.github.com>
2021-11-01 18:42:13 +0000

    Raw skeleton of a program.

---
+++ main.py
@@ -0,0 +1,14 @@
+#flask vid: https://www.youtube.com/watch?v=Z1RJmh_OqeA
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def main():
+  return render_template("main.html")
+
+def testing():
+  if request.method == "POST":
+    thing = request.form["input"]
+    return render_template("test.html", thing = thing)
